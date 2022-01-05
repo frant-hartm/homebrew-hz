5f6f0d74944a5689e9d3c9fe558f41586068e201
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-05 22:57:17 +0100

    Test

---
+++ hazelcast@5.1.SNAPSHOT.rb
@@ -0,0 +1,29 @@
+class HazelcastAT51SNAPSHOT < Formula
+    desc "Tool to run Hazelcast IMDG member instances locally"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://oss.sonatype.org/content/repositories/snapshots/hazelcast-distribution/hazelcast-5.1-SNAPSHOT.tar.gz"
+    sha256 "fc9dd2c3a8df1279fe7f77021d4f1d2a885ac2491e2134db595e1d7909032087"
+  
+    bottle :unneeded
+  
+    depends_on "openjdk" => :recommended
+  
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"bin/download/hazelcast-download.properties", "hazelcastDownloadId=CLI", "hazelcastDownloadId=CLI_BREW"
+    end
+  
+    def post_install
+      exec "hz"
+    end
+  
+  end
+  
