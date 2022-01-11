6a6c18d56e13c49e322aa275a87fd3dda664fdfc
devOpsHazelcast <devops@hazelcast.com>
2022-01-11 22:20:50 +0000

    Hazelcast Command Line 5.0.1 release

---
+++ hazelcast@5.0.1.rb
@@ -0,0 +1,29 @@
+class HazelcastAT501 < Formula
+    desc "Tool to run Hazelcast IMDG member instances locally"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://repo1.maven.org/maven2/com/hazelcast/hazelcast-distribution/5.0.1/hazelcast-distribution-5.0.1.tar.gz"
+    sha256 "35db1aa877c0d9359bd3c412a3391b341d555598b0b352afa60b15a48ccea046"
+    conflicts_with "hazelcast-enterprise"
+  
+    depends_on "openjdk" => :recommended
+  
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"lib/hazelcast-download.properties", "hazelcastDownloadId=distribution", "hazelcastDownloadId=brew"
+    end
+  
+    def post_install
+      # TODO fix once we have hz command in main distro
+      exec "echo Hazelcast installed"
+    end
+  
+  end
+  
